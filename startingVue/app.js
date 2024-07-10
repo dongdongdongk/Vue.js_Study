d975328edfe23014a20834fb4c56bf257e005beb
dhk939393 <dhk9309@gmail.com>
2024-07-10 10:24:49 +0900

    vue

--- startingVue/app.js
+++ startingVue/app.js
@@ -1,8 +1,20 @@
 const app = Vue.createApp({
     data : function(){
         return {
-            courseGoal : '뷰를 마스터 하자!'
+            courseGoalA : '뷰를 마스터 하자!',
+            courseGoalB : '<h2>뷰를 마스터 하지 말자!</h2>',
+            vueLink : 'https://www.naver.com/'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if ( randomNumber < 0.5 ) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        }
     }
 });
 app.mount('#user-goal')
